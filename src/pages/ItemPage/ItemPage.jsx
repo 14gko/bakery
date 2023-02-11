9008d90d8dfb7e5370c9abe089974f5476b0e3ab
14gko <14gkoo@gmail.com>
2023-02-10 20:01:02 -0500

    add to itempage

--- src/pages/ItemPage/ItemPage.jsx
+++ src/pages/ItemPage/ItemPage.jsx
@@ -1,3 +1,26 @@
+import {useState, useEffect, useRef} from 'react';
+import * as itemsAPI from '../../utilities/items-api'
+import * as ordersAPI from '../../utilities/orders-api'
+import './ItemPage.css';
+
 export default function ItemPage(){
+    const [menuItems, setMenuItems] = useState([]);
+    const [cart, setCart] = useState(null);
+    const categoriesRef = useRef([]);
+
+    useEffect(function(){
+        async function getItems(){
+            const items = await itemsAPI.getAll();
+            //maybe add categoriesref
+            setMenuItems(items);
+        }
+        getItems();
+        async function getCart(){
+            const cart = await ordersAPI.getCart()
+            setCart(cart);
+        }
+        getCart();
+    })
+
     return;
 }
